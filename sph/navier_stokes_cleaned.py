ad031abe7e9c2486ef3c0929d722d28f1e260e6f
Umut Oktem <umbutcan@gmail.com>
2017-11-06 23:39:15 -0800

    Update navier_stokes_cleaned.py

--- sph/navier_stokes_cleaned.py
+++ sph/navier_stokes_cleaned.py
@@ -80,6 +80,28 @@ def kroupa_imf(base_imf):
     
     return (imf_final)
 
+
+
+def viscosity_2(i):
+    accel = 0 #initial acceleration (force) due to viscosity
+    indices_nearest = neighbor[i]
+    for j in range(len(indices_nearest)):
+        if(density(i)==0):
+            continue
+        c1 = 0.5 #not the actual value
+        c2 = 0.7 #not the actual value
+        alpha = 1
+        rho_ij = (density(j)+density(i))/2
+        w_ij = np.dot((points[j]-points[i]),(velocities[j]-velocities[i]))/np.dot((points[j]-points[i]),(points[j]-points[i]))
+        v_ij = c1+c2-3*w_ij
+        if (w_ij < 0):
+            factor = -alpha*v_ij*w_ij/(2*rho_ij)
+        else:
+            factor = 0
+        accel += -mass[j]*factor*grad_weight(points[i], points[j], mass[i],particle_type[i])
+    return(accel)
+        
+	
 def mu_j(j): #mean molecular weight of the SPH particle
     #species = 13
     mu = 0
