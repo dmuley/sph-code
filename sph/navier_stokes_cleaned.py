Fixed yield calculation and unit conversions.

sph/navier_stokes_cleaned.py
@@ -649,14 +649,15 @@ def chemisputtering(points, neighbor, mass, f_un, mu_array, sizes, T, particle_t
 			Y_H = min(max(0.5 * np.exp(-4600/sph_temperature), 1e-7),1e-3) * sputtering_yields/max(sputtering_yields)
 			Y_He = min(max(5 * np.exp(-4600/sph_temperature), 1e-6),1e-2) * sputtering_yields/max(sputtering_yields)
 			K_u = sph_composition_density + dust_composition - sph_composition_density[0] * Y_H - sph_composition_density[1] * Y_He
-			L_u = sph_composition_density + dust_composition * np.exp(K_u * J_u * dt) - sph_composition_density[0] * Y_H - sph_composition_density[1] * Y_He
+			L_u = sph_composition_density + dust_composition * np.exp(K_u * J_u * dt) - sph_composition_density[3] * Y_H - sph_composition_density[4] * Y_He
+			#yields are for ions!
 			
 			F_sput = K_u/L_u
 			F_sput[np.isnan(F_sput)] = 1.
 			F_sput *= np.exp(K_u * J_u * dt)
 			
 			#How to weight the amount of material deposited wherever?
-			effective_mass = -(sph_indiv_composition - np.outer(sph_indiv_composition.T[0], Y_H) - np.outer(sph_indiv_composition.T[1], Y_He))
+			effective_mass = -(sph_indiv_composition - np.outer(sph_indiv_composition.T[3], Y_H) - np.outer(sph_indiv_composition.T[4], Y_He))
 			
 			#should be: (MOLECULES IN DUST SPH PARTICLES/MOLECULES IN GAS SPH PARTICLES)
 			frac_gained = np.sum(dust_composition)/np.sum(sph_composition_density)
@@ -665,9 +666,9 @@ def chemisputtering(points, neighbor, mass, f_un, mu_array, sizes, T, particle_t
 			reuptake_weight = np.nan_to_num(effective_mass/np.sum(effective_mass,axis=0))
 			
 			dest_frac = (((1. - F_sput) * f_un[neighbor[j]]).T * ((w2d > 0) & (particle_type[neighbor[j]] == 2))).T
-			df = np.sum(dest_frac.T * (mass[neighbor[j]]/mu_array[neighbor[j]]), axis=1).T
+			df = np.sum(dest_frac.T * (mass[neighbor[j]]/mu_array[neighbor[j]]), axis=1).T/amu
 			
-			frac_destruction[neighbor[j]] += (dest_frac.T * (mass[neighbor[j]]/mu_array[neighbor[j]])).T
+			frac_destruction[neighbor[j]] += (dest_frac.T * (mass[neighbor[j]]/mu_array[neighbor[j]])).T/amu
 			frac_reuptake[neighbor[j]] += reuptake_weight * df
 	
 	frac_destruction = (frac_destruction.T/(mass/(mu_array * amu))).T